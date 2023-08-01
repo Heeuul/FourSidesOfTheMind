f100af8e5cea0a2e6169d224a10326f73457331f
Hans <aimanhans@yahoo.com>
2023-08-01 17:21:09 +0800

    010823 - 17:20 - Home Page Contents

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,8 +5,8 @@ import {
   StyleSheet,
   FlatList,
   TouchableOpacity,
+  Linking,
 } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
 
 import useType from "../hooks/useType";
@@ -14,52 +14,154 @@ import useType from "../hooks/useType";
 export default function HomeScreen({ navigation }) {
   const { FlipEnergy, FlipPerceive, FlipJudge, FlipPreference, GetType } =
     useType();
-  const type = GetType("Ego");
+  const side = "Ego";
+  const type = GetType(side);
 
-  function RenderLetterButton(letter) {
+  function RenderLetterButton(letter, index) {
     return (
-      <TouchableOpacity>
-        <LinearGradient
-          colors={["#fff", "#000"]}
-          style={styles.letterContainer}
-        >
-          <Text style={styles.letterText}>{letter}</Text>
-        </LinearGradient>
+      <TouchableOpacity
+        onPress={() => FlipFunction(index)}
+        style={styles.letterButton}
+      >
+        <Text style={styles.letterText}>{letter}</Text>
       </TouchableOpacity>
     );
+  }
+  function FlipFunction(index) {
+    switch (index) {
+      case 0:
+        FlipEnergy();
+        return;
+      case 1:
+        FlipPerceive();
+        return;
+      case 2:
+        FlipJudge();
+        return;
+      case 3:
+        FlipPreference();
+        return;
+    }
   }
 
   return (
     <View style={{ flex: 1 }}>
-      <Text style={styles.desireText}>Enter desired type</Text>
+      <Text style={styles.headerText}>Desired type</Text>
+
       <View style={{ alignItems: "center" }}>
         <FlatList
           data={type}
-          renderItem={({ item }) => RenderLetterButton(item)}
+          renderItem={({ item, index }) => RenderLetterButton(item, index)}
           scrollEnabled={false}
           horizontal={true}
         />
       </View>
-      <Button
-        title="Go to Main"
-        onPress={() => navigation.navigate("4Sides")}
-      />
+
+      <View style={{ flex: 1 }}>
+        <TouchableOpacity
+          style={styles.navButton}
+          onPress={() => navigation.navigate("4Sides")}
+        >
+          <Text adjustsFontSizeToFit={true} style={styles.navText}>
+            Show Four Sides
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.navButton}
+          onPress={() => navigation.navigate("1Side", { side })}
+        >
+          <Text adjustsFontSizeToFit={true} style={styles.navText}>
+            Show Details
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={{ flex: 2, width: "80%", alignSelf: "center" }}>
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 50,
+            textDecorationLine: "underline",
+          }}
+        >
+          Resources
+        </Text>
+        <TouchableOpacity
+          style={styles.refButton}
+          onPress={() => Linking.openURL("https://www.udja.app/")}
+        >
+          <Text adjustsFontSizeToFit={true} style={styles.refText}>
+            Identify your type
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.refButton}
+          onPress={() => Linking.openURL("https://offers.csjoseph.life/famous")}
+        >
+          <Text adjustsFontSizeToFit={true} style={styles.refText}>
+            Types of famous people
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.refButton}
+          onPress={() => Linking.openURL("https://csjoseph.life")}
+        >
+          <Text adjustsFontSizeToFit={true} style={styles.refText}>
+            Advanced Content
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.refButton}
+          onPress={() =>
+            Linking.openURL("https://discord.com/invite/egohackers")
+          }
+        >
+          <Text adjustsFontSizeToFit={true} style={styles.refText}>
+            Community Discord
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  desireText: {
-    fontSize: 35,
+  headerText: {
+    fontSize: 45,
     textAlign: "center",
   },
-  letterContainer: {
-    margin: 5,
+  letterButton: {
+    width: 90,
+    aspectRatio: 1,
+    justifyContent: "center",
     alignItems: "center",
+    borderRadius: 25,
+    backgroundColor: "black",
+    margin: 2,
   },
   letterText: {
-    fontSize: 100,
+    fontSize: 75,
     textAlign: "center",
-    borderBottomWidth: 2,
+    textAlignVertical: "center",
+    color: "white",
+    includeFontPadding: false,
+  },
+  navButton: {
+    borderWidth: 2,
+    borderRadius: 25,
+    margin: 5,
+  },
+  navText: {
+    textAlign: "center",
+    fontSize: 35,
+  },
+  refButton: {
+    borderWidth: 2,
+    borderRadius: 10,
+    margin: 5,
+  },
+  refText: {
+    textAlign: "center",
+    fontSize: 25,
   },
 });
